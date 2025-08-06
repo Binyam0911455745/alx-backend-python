1efb28ebd1779a2df2aecd97a7d5742ae11444b3
binyam <binyamwork@gmail.com>
2025-08-07 01:33:00 +0300

    f' this is commiting a file messaging_app

--- messaging_app/settings.py
+++ messaging_app/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta # Added for SIMPLE_JWT configuration
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,8 +84,12 @@ WSGI_APPLICATION = 'messaging_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME', 'messaging_app_db'),
+        'USER': os.getenv('DB_USER', 'django_user'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'your_secure_password'),
+        'HOST': 'db', # This links to the name of the database service in docker-compose.yml
+        'PORT': '3306',
     }
 }
 
